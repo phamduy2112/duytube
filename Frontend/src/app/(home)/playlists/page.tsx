8f6edf293942f953275f26b5de9cd2a888926ca3
Duy <duyp7454@gmal.com>
2025-05-14 01:11:49 -0700

    update frontend again

--- Frontend/src/app/(home)/playlists/page.tsx
+++ Frontend/src/app/(home)/playlists/page.tsx
@@ -3,6 +3,7 @@ import { PlayListsView } from "@/modules/playlists/ui/views/playlists-view"
 const Page=()=>{
     return (
         <div className="py-17">
+            {/* asdasd */}
             <PlayListsView/>
         </div>
     )
