5c6afa72c985c6db545c9b665e609fe9134b849d
Duy <duyp7454@gmal.com>
2025-05-30 09:40:23 -0700

    update code frontend deloy

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_FRONTEND_API: "pk_test_cmFwaWQtYmF0LTQuY2xlcmsuYWNjb3VudHMuZGV2JA",
+    CLERK_SECRET_KEY: "sk_test_Tr71vVJ9SydOVEl5nEKpWCzmzem9QX1h4bVSF9Pg8c",
   },
 };
 
