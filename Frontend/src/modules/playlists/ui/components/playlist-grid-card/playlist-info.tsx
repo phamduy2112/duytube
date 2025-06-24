5cf0d76731e7942484d4cf450bee760e914777b8
Duy <duyp7454@gmal.com>
2025-06-24 07:46:37 -0700

    update code backend deloy

--- Frontend/src/modules/playlists/ui/components/playlist-grid-card/playlist-info.tsx
+++ Frontend/src/modules/playlists/ui/components/playlist-grid-card/playlist-info.tsx
@@ -11,6 +11,9 @@ export const PlaylistInfo=({
                     <p className="text-sm text-muted-foreground">Playlist</p>
                     <p className="text-sm text-muted-foreground font-semibold hover:text-primary">View full playlist</p>
                 </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-vertical-icon lucide-ellipsis-vertical"><circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/></svg>
+                </div>
         </div>
     )
 }
