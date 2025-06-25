0eceff50518f33724433701318c22f5b3f6abb35
Duy <duyp7454@gmal.com>
2025-06-24 21:43:32 -0700

    update code frontend

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     domains: ['image.mux.com'],
   },
   env: {
-    NEXT_PUBLIC_CLERK_FRONTEND_API: "pk_test_cmFwaWQtYmF0LTQuY2xlcmsuYWNjb3VudHMuZGV2JA",
+    CLERK_PUBLISHABLE_KEY: "pk_test_cmFwaWQtYmF0LTQuY2xlcmsuYWNjb3VudHMuZGV2JA",
     CLERK_SECRET_KEY: "sk_test_Tr71vVJ9SydOVEl5nEKpWCzmzem9QX1h4bVSF9Pg8c",
     
   },
